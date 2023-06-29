d0991196d9cf14bda207c08fdbde223b51890c1b
JehanKandy <jehankandy@gmail.com>
2023-06-30 00:11:33 +0530

    Update

--- resources/views/comments/create.blade.php
+++ resources/views/comments/create.blade.php
@@ -4,5 +4,14 @@
     <div class="admin-card">
         <a href="{{ url('/comments') }}"><button class="btn btn-primary">Back</button></a><br><br>
         <h3>Add New Comment</h3>
+        <hr>
+        <br>
+        <form action="{{ url('/comments') }}" method="POST">
+            @csrf
+            <div class="form-group">
+                <label for="add_user">Owner</label>
+                <input type="text" name="add_user" class="form-control" value="@php Auth::user->name @endphp" disabled>
+            </div>
+        </form>
     </div>
 @endsection
