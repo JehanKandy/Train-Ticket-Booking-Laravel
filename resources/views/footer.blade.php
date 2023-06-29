592fc2ce8cf7c144b574160c697cf2ec783303c1
JehanKandy <jehankandy@gmail.com>
2023-06-29 22:45:43 +0530

    Update

--- resources/views/footer.blade.php
+++ resources/views/footer.blade.php
@@ -4,19 +4,26 @@
         color: white;
         padding: 50px 0;
     }
+    h3{
+        text-align: center;
+        padding: 0 0 20px 0;
+    }
+    .footer-text{
+        text-align: center;
+    }
 </style>
 
 <footer>
     <div class="container">
         <h3>Train Ticket Booking</h3>
         <div class="row">
-            <div class="col-lg-4">
+            <div class="col-lg-4 footer-text">
                 Footer 1
             </div>
-            <div class="col-lg-4">
+            <div class="col-lg-4 footer-text">
                 Footer 1
             </div>
-            <div class="col-lg-4">
+            <div class="col-lg-4 footer-text">
                 Footer 1
             </div>
         </div>
