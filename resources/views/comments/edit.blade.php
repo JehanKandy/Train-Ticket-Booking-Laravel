452c71c7d5a55c5f02b5d1a709770670cb82a7f4
JehanKandy <jehankandy@gmail.com>
2023-06-30 00:59:47 +0530

    Update

--- resources/views/comments/edit.blade.php
+++ resources/views/comments/edit.blade.php
@@ -3,5 +3,18 @@
 @section('content')
     <div class="admin-card">
         <a href="{{ url('/comments') }}"><button class="btn btn-primary">Back</button></a><br><br>
+        <h2>Update Comment</h2>
+        <hr>
+
+        <form action="{{ url('users/' .$members->id) }}" method="POST">
+            @csrf
+            @method("PATCH")
+            <div class="form-group">
+                <label for="Owner">Owner</label>
+                <input type="text" name="add_user" value="{{ $comments->add_user }}" class="form-control">
+            </div>
+        </form>
+
+        
     </div>
 @endsection
