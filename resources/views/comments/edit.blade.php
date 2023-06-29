0c0d1565eddab5672d5cdf9b139316adc8d6a8e4
JehanKandy <jehankandy@gmail.com>
2023-06-30 01:01:20 +0530

    Update

--- resources/views/comments/edit.blade.php
+++ resources/views/comments/edit.blade.php
@@ -11,7 +11,12 @@
             @method("PATCH")
             <div class="form-group">
                 <label for="Owner">Owner</label>
-                <input type="text" name="add_user" value="{{ $comments->add_user }}" class="form-control">
+                <input type="text" value="{{ $comments->add_user }}" class="form-control" disabled><br>
+                <input type="hidden" name="add_user">
+            </div>
+            <div class="form-group">
+                <label for="Comment">Comment</label>
+                <textarea name="" id="" cols="30" rows="10"></textarea>
             </div>
         </form>
 
