3dc74b2cd68eb7a23f2b516f59abdbe6ccbb4c2d
JehanKandy <jehankandy@gmail.com>
2023-06-29 23:56:26 +0530

    Update

--- resources/views/comments/index.blade.php
+++ resources/views/comments/index.blade.php
@@ -1,5 +1,9 @@
 @extends('layouts.dash')
 
 @section('content')
-    
+    <div class="admin-card">
+        <h2>All Comments</h2>
+        <hr>
+        <a href="{{ url('comments/create') }}"><button class="btn btn-success"><i class="fas fa-plus"></i> Add New Comment</button></a>
+    </div>
 @endsection
