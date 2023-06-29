964d19acf6be63da98cc36e82315fadb63890cc1
JehanKandy <jehankandy@gmail.com>
2023-06-29 23:48:45 +0530

    Update

--- app/Models/Comments.php
+++ app/Models/Comments.php
@@ -8,4 +8,7 @@ use Illuminate\Database\Eloquent\Model;
 class Comments extends Model
 {
     use HasFactory;
+    protected $table = 'comments';
+    protected $primaryKey = 'id';
+    protected $fillable = ['add_user', 'comment', 'created_at', 'updated_at'];
 }
