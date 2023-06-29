78475ce257cd844da80fcb537bd342d0999eb716
JehanKandy <jehankandy@gmail.com>
2023-06-29 23:39:05 +0530

    Update

--- resources/views/layouts/dash.blade.php
+++ resources/views/layouts/dash.blade.php
@@ -101,7 +101,7 @@
             <a href="{{url('/users')}}" class="list-group-item list-group-item-action bg-transparent second-text fw-bold"><i
                     class="fas fa-user-tie me-2"></i>Users</a>
             <a href="{{url('/comments')}}" class="list-group-item list-group-item-action bg-transparent second-text fw-bold"><i
-                    class="fas fa-user-tie me-2"></i>Comments</a>
+                    class="fas fa-comments me-2"></i>Comments</a>
 
             
         </div>
