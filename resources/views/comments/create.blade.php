58b96305e85e67e5c4e8b1dc85dcd5cd56c0fb00
JehanKandy <jehankandy@gmail.com>
2023-06-30 00:02:22 +0530

    Update

--- resources/views/comments/create.blade.php
+++ resources/views/comments/create.blade.php
@@ -3,5 +3,6 @@
 @section('content')
     <div class="admin-card">
         <a href="{{ url('/comments') }}"><button class="btn btn-primary">Back</button></a><br><br>
+        <h3>Add New Comment</h3>
     </div>
 @endsection
