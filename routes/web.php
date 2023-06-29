3d836976000177cc2562893349e44592f312cdda
JehanKandy <jehankandy@gmail.com>
2023-06-29 23:52:23 +0530

    Update

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\CommentController;
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Route;
 
@@ -39,3 +40,4 @@ Route::get('client', function(){
 })->name('client')->middleware('client');
 
 Route::resource("/users", UserController::class);
+Route::resource('/comments', CommentController::class);
