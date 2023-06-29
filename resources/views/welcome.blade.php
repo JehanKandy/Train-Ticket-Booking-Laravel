877dd6e8410f61d1eb28a93872b943a393fbe2c4
JehanKandy <jehankandy@gmail.com>
2023-06-29 22:38:31 +0530

    Update

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -2,7 +2,7 @@
 <style>
     .home-content{
         height: 80vh;
-        background-image: linear-gradient(to bottom, rgba(78, 78, 78, 0.52), rgba(0, 0, 0, 0.73)), url('https://wallpapercave.com/dwp1x/YKCVphS.jpg');
+        background-image: linear-gradient(to bottom, rgba(78, 78, 78, 0.52), rgba(0, 0, 0, 0.863)), url('https://wallpapercave.com/dwp1x/YKCVphS.jpg');
         background-position: center;
         background-repeat: no-repeat;
         background-size: cover;
@@ -10,10 +10,29 @@
     .main-title{
         color: white;
         text-align: center;
-        padding: 200px 0;
+        padding: 200px 0 0 0;
         font-size: 50px;
     }
-    
+    .main-body{
+        text-align: center;
+        color: white;
+        font-size: 25px;
+    }
+    .book-btn{
+        background: transparent;
+        border: 1px solid white;
+        border-radius: 5px;
+        margin: 25px 0;
+        padding: 15px 30px; 
+        color: white;
+        transition: 0.5s;
+    }
+    .book-btn:hover{
+        cursor: pointer;
+        color: black;
+        background-color: white;
+        box-shadow: 10px 10px 8px 10px #888888;
+    }
 </style>
 
 @include('header')
@@ -47,6 +66,10 @@
 <div class="home-content">
     <div class="container">
         <h1 class="main-title">Book here And Travel AnyWhere</h1>
+        <h4 class="main-body">You can book a train before 1hour it's departure time <br>
+
+        <a href=""><button class="book-btn"><i class="fas fa-train"></i> Book A Train</button></a>
+    </h4>
     </div>
 </div>
 
