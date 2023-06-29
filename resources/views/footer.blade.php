d5af201e45098ff33795ecb5d642508e401ba756
JehanKandy <jehankandy@gmail.com>
2023-06-29 22:44:06 +0530

    Update

--- resources/views/footer.blade.php
+++ resources/views/footer.blade.php
@@ -2,11 +2,13 @@
     footer{
         background-color: black;
         color: white;
+        padding: 50px 0;
     }
 </style>
 
 <footer>
     <div class="container">
+        <h3>Train Ticket Booking</h3>
         <div class="row">
             <div class="col-lg-4">
                 Footer 1
