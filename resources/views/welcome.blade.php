f0ad76056b5d0914c2c494c2ac24d82710422bbf
JehanKandy <jehankandy@gmail.com>
2023-06-29 19:13:19 +0530

    Update

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,5 +1,11 @@
 
-    @include('header')
+<style>
+    .home-content{
+        height: 60vh;
+    }
+</style>
+
+@include('header')
 
     {{-- <body class="antialiased">
         <div class="relative sm:flex sm:justify-center sm:items-center min-h-screen bg-dots-darker bg-center bg-gray-100 dark:bg-dots-lighter dark:bg-gray-900 selection:bg-red-500 selection:text-white">
@@ -26,6 +32,12 @@
 
             @endif            
         </div> --}}
+
+<div class="home-content">
+    <div class="container">
+        Book here And Travel AnyWhere
+    </div>
+</div>
 
 
 
