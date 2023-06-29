44ad34a4af433e3273daf00394c2c813d7781fac
JehanKandy <jehankandy@gmail.com>
2023-06-30 00:48:40 +0530

    Update

--- resources/views/comments/show.blade.php
+++ resources/views/comments/show.blade.php
@@ -3,10 +3,10 @@
 @section('content')
     <div class="admin-card">
         <a href="{{ url('/comments') }}"><button class="btn btn-primary">Back</button></a><br><br>
-        Comment
+        <h2>Comment</h2>
         <br>
-        <h3>Owner</h3>
-        <h5>{{ $comments->add_user }}</h5>
+        <span><h3>Owner : </h3> {{ $comments->add_user }}</span>
+
 
     </div>
 @endsection
