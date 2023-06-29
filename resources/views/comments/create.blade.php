310099d4cee9c502242fe3c62cf8f0b6a5e3359c
JehanKandy <jehankandy@gmail.com>
2023-06-30 00:01:52 +0530

    Update

--- resources/views/comments/create.blade.php
+++ resources/views/comments/create.blade.php
@@ -1,5 +1,7 @@
 @extends('layouts.dash')
 
 @section('content')
-    
+    <div class="admin-card">
+        <a href="{{ url('/comments') }}"><button class="btn btn-primary">Back</button></a><br><br>
+    </div>
 @endsection
