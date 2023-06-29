e4f3bc370cf27be6664c64bedb8dcff71a2dead9
JehanKandy <jehankandy@gmail.com>
2023-06-29 23:15:59 +0530

    Update

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -84,12 +84,13 @@
     <div class="sec-content">
         <div class="container">
             <div class="row">
-                <div class="col-lg-6">
+                <div class="col-lg-5">
                     <div class="comment-title">
                         Users Comments
                     </div>
                 </div>
-                <div class="col-lg-6">
+                <div class="col-lg-2"></div>
+                <div class="col-lg-5">
                     <div class="news-title">
                         Latest NEWS
                     </div>
