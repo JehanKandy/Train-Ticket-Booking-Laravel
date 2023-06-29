fd98964eab0189b5bd10f250d3f8c310002f1eda
JehanKandy <jehankandy@gmail.com>
2023-06-29 23:57:47 +0530

    Update

--- resources/views/comments/index.blade.php
+++ resources/views/comments/index.blade.php
@@ -5,5 +5,17 @@
         <h2>All Comments</h2>
         <hr>
         <a href="{{ url('comments/create') }}"><button class="btn btn-success"><i class="fas fa-plus"></i> Add New Comment</button></a>
+        <br><br>
+
+        <table class="table">
+            <thead class="table-primary">
+                <tr>
+                    <th>ID</th>
+                    <th>Owner</th>
+                    <th>Comment</th>
+                    <th>Action</th>
+                </tr>
+            </thead>
+        </table>
     </div>
 @endsection
