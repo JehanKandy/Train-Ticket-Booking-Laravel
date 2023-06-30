2b2dbdba6658859f76710181f73f7d6442ea4b74
JehanKandy <jehankandy@gmail.com>
2023-06-30 13:26:25 +0530

    Update

--- app/Models/TrainRoutes.php
+++ app/Models/TrainRoutes.php
@@ -7,5 +7,8 @@ use Illuminate\Database\Eloquent\Model;
 
 class TrainRoutes extends Model
 {
+    protected $table = 'train_routes';
+    protected $primaryKey = 'id';
+    protected $fillable = ['train_name', 'start_station', 'start_time', 'end_station' , 'end_time', 'created_at', 'updated_at'];
     use HasFactory;
 }
