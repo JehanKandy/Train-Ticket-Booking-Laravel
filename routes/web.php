610c354bc1044be59e2925ca1f7ce59ba3fd5969
JehanKandy <jehankandy@gmail.com>
2023-06-30 13:59:50 +0530

    Update

--- routes/web.php
+++ routes/web.php
@@ -5,6 +5,7 @@ use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Route;
 
 use App\Http\Controllers\UserController;
+use App\Http\Controllers\TrainRouteController;
 
 /*
 |--------------------------------------------------------------------------
