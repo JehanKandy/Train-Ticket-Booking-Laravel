fe7b7ac3f127d415184b5e5fa166b8cf1d8e61e9
JehanKandy <jehankandy@gmail.com>
2023-07-01 09:27:09 +0530

    Update

--- resources/views/train_routes/index.blade.php
+++ resources/views/train_routes/index.blade.php
@@ -3,7 +3,24 @@
 @section('content')
     <div class="admin-card">
         @if (auth()->user()->role == 1)
-            <a href="{{ url('train_routes/create') }}"><button class="btn btn-success"><i class="fas fa-subway"> Add Train Route</i></button></a><br><br>
+            <a href="{{ url('train_routes/create') }}"><button class="btn btn-success"><i class="fas fa-subway"></i> Add Train Route</button></a><br>
+            <hr><br><br>
+            <table class="table">
+                <thead class="table-primary">
+                    <tr>
+                        <th>ID</th>
+                        <th>Train Name</th>
+                        <th>Start Station</th>
+                        <th>Start Time</th>
+                        <th>End Station</th>
+                        <th>End Time</th>
+                        <th>Add Date</th>
+                        <th>Last Update</th>
+                        <th>Action</th>
+                    </tr>
+                </thead>
+            </table>
         @endif
+
     </div>
 @endsection
