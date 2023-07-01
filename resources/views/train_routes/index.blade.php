6fca5a7ca12df4b446cd030e952cb4190acab37e
JehanKandy <jehankandy@gmail.com>
2023-07-01 09:24:16 +0530

    Update

--- resources/views/train_routes/index.blade.php
+++ resources/views/train_routes/index.blade.php
@@ -3,7 +3,7 @@
 @section('content')
     <div class="admin-card">
         @if (auth()->user()->role == 1)
-            <a href="{{ url('train_routes/create') }}"><button class="btn btn-success"><i class="fas fa-subway"></i></button></a>
+            <a href="{{ url('train_routes/create') }}"><button class="btn btn-success"><i class="fas fa-subway"> Add Train Route</i></button></a><br><br>
         @endif
     </div>
 @endsection
