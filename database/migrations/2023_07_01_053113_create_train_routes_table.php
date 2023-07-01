9a6b3ccfd289280ff388136d470bc38c8471f9ee
JehanKandy <jehankandy@gmail.com>
2023-07-01 11:01:31 +0530

    Update

---
+++ database/migrations/2023_07_01_053113_create_train_routes_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('train_routes', function (Blueprint $table) {
+            $table->id();
+            $table->string('train_name')->unique();
+            $table->string('start_station');
+            $table->time('start_time');
+            $table->string('end_station');
+            $table->time('end_time');
+            $table->json('weekly_schedule');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('train_routes');
+    }
+};
