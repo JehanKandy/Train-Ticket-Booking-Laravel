93f27e7152030859b47dfcaf952d89d4065b0f95
JehanKandy <jehankandy@gmail.com>
2023-07-01 10:00:56 +0530

    Update

--- app/Models/TrainRoutes.php
+++ app/Models/TrainRoutes.php
@@ -9,6 +9,6 @@ class TrainRoutes extends Model
 {
     protected $table = 'train_routes';
     protected $primaryKey = 'id';
-    protected $fillable = ['train_name', 'start_station', 'start_time', 'end_station' , 'end_time', 'created_at', 'updated_at'];
+    protected $fillable = ['train_name', 'start_station', 'start_time', 'end_station' , 'end_time', 'weekly_schedule', 'created_at', 'updated_at'];
     use HasFactory;
 }
