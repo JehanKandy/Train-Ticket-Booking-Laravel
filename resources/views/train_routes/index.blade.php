c569675e9ec887016d26caadb461949ae55184e5
JehanKandy <jehankandy@gmail.com>
2023-07-01 12:52:28 +0530

    Update

--- resources/views/train_routes/index.blade.php
+++ resources/views/train_routes/index.blade.php
@@ -41,8 +41,22 @@
                         <td>{{ $train->created_at }}</td>
                         <td>{{ $train->updated_at }}</td>
                         <td>
+                            <a href="{{ url('/train_routes/' . $comment->id) }}" title="View Comments"><button class="btn btn-info btn-sm"><i class="fa fa-eye" aria-hidden="true"></i> View</button></a>
+                            
+                            @if (auth()->user()->role == 1 || auth()->user()->role == 2 || auth()->user()->name == $comment->add_user)
+                                <a href="{{ url('/train_routes/' . $comment->id . '/edit') }}" title="Edit Train Routes"><button class="btn btn-primary btn-sm"><i class="fas fa-edit" aria-hidden="true"></i> Edit</button></a>
+                            @endif
+
+                            @if (auth()->user()->role == 1 || auth()->user()->name == $comment->add_user)
+                                <form method="POST" action="{{ url('/comments' . '/' . $comment->id) }}" accept-charset="UTF-8" style="display:inline">
+                                    {{ method_field('DELETE') }}
+                                    {{ csrf_field() }}
+                                    <button type="submit" class="btn btn-danger btn-sm" title="Delete train routes" onclick="return confirm(&quot;Confirm delete?&quot;)"><i class="fas fa-calendar-times" aria-hidden="true"></i> Delete</button>
+                                </form>                            
+                            @endif
                             
                         </td>
+                    </tr> 
                     @endforeach
                 </tbody>
             </table>
