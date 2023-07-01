4a222f445fa1ad387af9daf7567654cf0d9600aa
JehanKandy <jehankandy@gmail.com>
2023-07-01 09:54:25 +0530

    Update

--- resources/views/train_routes/create.blade.php
+++ resources/views/train_routes/create.blade.php
@@ -8,7 +8,15 @@
         <h2><i class="fas fa-subway"></i> Add Train Route</h2>
 
         <form action="{{ url('/train_routes') }}" method="post">
-        
+            @csrf
+            <div class="form-group">
+                <label for="train_name">Train Name</label>
+                <input type="text" name="train_name" class="form-control" placeholder="Train Name">
+            </div>
+            <div class="form-group">
+                <label for="start_station">Start Station</label>
+                <input type="text" name="start_station" class="form-control" placeholder="Start Station">
+            </div>
         </form>
         <hr>
     </div>
