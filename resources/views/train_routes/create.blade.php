c234a3cc4b48d666919b216bdedb170931ebd4a4
JehanKandy <jehankandy@gmail.com>
2023-07-01 09:52:37 +0530

    Update

--- resources/views/train_routes/create.blade.php
+++ resources/views/train_routes/create.blade.php
@@ -3,7 +3,13 @@
 
 @section('content')
     <div class="admin-card">
+        <a href="{{ url('/train_routes') }}"><button class="btn btn-primary">Back</button></a><br><br>
+
         <h2><i class="fas fa-subway"></i> Add Train Route</h2>
+
+        <form action="{{ url('/train_routes') }}" method="post">
+        
+        </form>
         <hr>
     </div>
 @endsection
