44bd96717deb654a14304ce6c88ff8574ae9733e
JehanKandy <jehankandy@gmail.com>
2023-07-01 11:10:40 +0530

    Update

--- resources/views/train_routes/create.blade.php
+++ resources/views/train_routes/create.blade.php
@@ -15,7 +15,7 @@
             <ul>
                 @foreach ($errors->all() as $error)
                     <div class="alert alert-danger alert-dismissible fade show" role="alert">
-                        <strong>Holy guacamole!</strong> You should check in on some of those fields below.
+                        <strong>Errors : </strong> You got Following Errors while 
                         <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>
                     </div>
                 @endforeach
