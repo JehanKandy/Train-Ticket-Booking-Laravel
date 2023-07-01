20fb6b37e16bff8f3c093f590ea3c1f640603c37
JehanKandy <jehankandy@gmail.com>
2023-07-01 13:02:17 +0530

    Update

--- resources/views/train_routes/show.blade.php
+++ resources/views/train_routes/show.blade.php
@@ -3,5 +3,13 @@
 @section('content')
     <div class="admin-card">
         <h2>Train Route</h2>
+        <div class="row">
+            <div class="col-lg-6">
+                Train Name
+            </div>
+            <div class="col-lg-6">
+                {{ $train_routes->train_name }}
+            </div>
+        </div>
     </div>
 @endsection
