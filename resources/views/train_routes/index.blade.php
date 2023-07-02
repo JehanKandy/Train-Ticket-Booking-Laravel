0c64fc62aa2fdc5cdbe02af8a2e2795c17b09808
JehanKandy <jehankandy@gmail.com>
2023-07-02 20:08:15 +0530

    Update

--- resources/views/train_routes/index.blade.php
+++ resources/views/train_routes/index.blade.php
@@ -57,7 +57,7 @@
                             @endif                            
                         </td>
                         <td>
-                            Gate Open
+                            
                         </td>
                     </tr> 
                     @endforeach
