4a25cc6f6c4ddd073ec05b6de479a0ac7d710f66
JehanKandy <jehankandy@gmail.com>
2023-07-02 17:51:45 +0530

    Update

--- app/Http/Controllers/TrainRouteController.php
+++ app/Http/Controllers/TrainRouteController.php
@@ -75,7 +75,15 @@ class TrainRouteController extends Controller
      */
     public function update(Request $request, string $id)
     {
-        //
+        $request->validate([
+            'train_name' => 'required|unique:train_routes,train_name|max:255',
+            'start_station' => 'required',
+            'start_time' => 'required',
+            'end_station' => 'required',
+            'end_time' => 'required',
+            'weekly_schedule' =>'required',
+        ]);
+        dd($request->all());
     }
 
     /**
