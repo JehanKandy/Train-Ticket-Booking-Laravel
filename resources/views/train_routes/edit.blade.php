c2870b6869e1f443a8a3e74f6e3176b1e39d7d01
JehanKandy <jehankandy@gmail.com>
2023-07-02 16:58:08 +0530

    Update

--- resources/views/train_routes/edit.blade.php
+++ resources/views/train_routes/edit.blade.php
@@ -1,5 +1,9 @@
 @extends('layouts.dash')
 
+@if (auth()->user()->role == 3)
+<script>window.location = "/train_routes";</script>
+@endif
+
 @section('content')
     <div class="admin-card">
         hi all
