b803c370a3664f299a38d5c31cee713141a1fa7e
JehanKandy <jehankandy@gmail.com>
2023-07-02 17:07:41 +0530

    Update

--- resources/views/train_routes/edit.blade.php
+++ resources/views/train_routes/edit.blade.php
@@ -28,7 +28,21 @@
                 <div class="col-lg-6">
                     <div class="form-group">
                         <label for="start_time">Start Station</label>
-                        <input type="time" name="start_time" id="" value="{{ $train_routes->start_time }}" class="form-control">
+                        <input type="time" name="start_time" id="" value="{{ $train_routes->start_time }}" class="form-control"><br>
+                    </div>
+                </div>
+            </div>
+            <div class="row">
+                <div class="col-lg-6">
+                    <div class="form-group">
+                        <label for="end_station">End Station</label>
+                        <input type="text" name="end_station" id="" value="{{ $train_routes->end_station }}" class="form-control">
+                    </div>
+                </div>
+                <div class="col-lg-6">
+                    <div class="form-group">
+                        <label for="start_time">Start Station</label>
+                        <input type="time" name="start_time" id="" value="{{ $train_routes->start_time }}" class="form-control"><br>
                     </div>
                 </div>
             </div>
