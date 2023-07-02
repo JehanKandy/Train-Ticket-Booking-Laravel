6f1b8381bf84a9f6ec0bd1387ee1503abbf54b11
JehanKandy <jehankandy@gmail.com>
2023-07-02 20:11:38 +0530

    Update

--- resources/views/layouts/dash.blade.php
+++ resources/views/layouts/dash.blade.php
@@ -97,7 +97,12 @@
         <div class="sidebar-heading text-center py-4 primary-text fs-4 fw-bold text-uppercase border-bottom"><i class="fa-sharp fa-solid fas fa-train"></i> Booking</div>
         <div class="list-group list-group-flush my-3">
             <a href="
-                    
+                    @php
+                        if(auth()->user()->role == 1){
+                            
+                        }
+                    @endphp
+
                     " class="list-group-item list-group-item-action bg-transparent second-text active"><i
                     class="fas fa-tachometer-alt me-2"></i>Dashboard</a>
             <a href="{{url('/users')}}" class="list-group-item list-group-item-action bg-transparent second-text fw-bold"><i
