432ba3beb02f3bd82ee1ea2f68d24e5f1698bc75
JehanKandy <jehankandy@gmail.com>
2023-07-02 20:53:20 +0530

    Update

--- resources/views/layouts/dash.blade.php
+++ resources/views/layouts/dash.blade.php
@@ -120,7 +120,7 @@
             <a href="" class="list-group-item list-group-item-action bg-transparent second-text active"><i
                     class="fas fa-tachometer-alt me-2"></i>
                     {{-- <span id="time"></span> --}}
-
+                    <span id = "clock" onload="currentTime()"></span>
                     </a>
 
             
@@ -194,7 +194,30 @@
     toggleButton.onclick = function () {
         el.classList.toggle("toggled");
     };
+    function currentTime() {
+  let date = new Date(); 
+  let hh = date.getHours();
+  let mm = date.getMinutes();
+  let ss = date.getSeconds();
+  let session = "AM";
 
+  if(hh == 0){
+      hh = 12;
+  }
+  if(hh > 12){
+      hh = hh - 12;
+      session = "PM";
+   }
 
+   hh = (hh < 10) ? "0" + hh : hh;
+   mm = (mm < 10) ? "0" + mm : mm;
+   ss = (ss < 10) ? "0" + ss : ss;
+    
+   let time = hh + ":" + mm + ":" + ss + " " + session;
+
+  document.getElementById("clock").innerText = time; 
+  let t = setTimeout(function(){ currentTime() }, 1000);
+}
+currentTime();
 
 </script>
